a01a4affbc034a25340bdcf40b744b6dbd24d004
Tan Ho <38083823+tanho63@users.noreply.github.com>
2020-04-12 13:06:04 -0400

    add new functions (#1)
    * clean up unbind desc
    
    * add fmt_dtcol and clean up other stuff

--- R/unbind_dt.R
+++ R/unbind_dt.R
@@ -1,8 +1,8 @@
 #' Unbind Shiny Inputs (UI function)
 #'
 #' When generating inputs reactively, it's sometimes necessary to unbind old inputs so that you can use the updated inputs in Shiny.
-#' `\code{"unbind_dt_js()"} needs to be called in the UI so that the JS code is available in the session,
-#' and then \code{unbind_dt()} should be called as needed in the server component (usually as part of an observeEvent or eventReactive)
+#' `\code{unbind_dt_js()} adds the JS to the UI, while \code{unbind_dt()} is called as needed in the server component
+#' (usually as part of an \code{observeEvent} or \code{eventReactive})
 #'
 #' @return Adds JS to HTML head
 #'
@@ -11,8 +11,8 @@
 #'
 #' @export
 unbind_dt_js <- function()
-  {htmltools::tags$head(
-    htmltools::tags$script(
+  {shiny::tags$head(
+    shiny::tags$script(
       'Shiny.addCustomMessageHandler("unbind_table_elements", function(x) {
               Shiny.unbindAll($(document.getElementById(x)).find(".dataTable"));
                         });'
@@ -20,12 +20,12 @@ unbind_dt_js <- function()
 
 #' Unbind Shiny Inputs (server function)
 #' When generating inputs reactively, it's sometimes necessary to unbind old inputs so that you can use the updated inputs in Shiny.
-#' `\code{"unbind_dt_js()"} needs to be called in the UI so that the JS code is available in the session,
-#' and then \code{unbind_dt()} should be called as needed in the server component (usually as part of an observeEvent or eventReactive)
+#' `\code{unbind_dt_js()} adds the JS to the UI, while \code{unbind_dt()} is called as needed in the server component
+#' (usually as part of an \code{observeEvent} or \code{eventReactive})
 #'
 #' @param dt_name String representing the table's output ID (i.e. to unbind in output$table_alpha, use "table_alpha" as the parameter)
 #'
-#' @return a call to the JS that unbinds tables.
+#' @return a call to the JS that unbinds \code{dt_name}
 #'
 #' @examples
 #' unbind_dt('player_table')
